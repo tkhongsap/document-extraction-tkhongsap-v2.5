042870f506fe048a1228ff57995b55fb1b2fc711
Chotiwat.tvpn <chotiwat.tvpn@tcc-technology.com>
2025-12-16 14:56:16 +0700

    export schemas

---
+++ backend/app/schemas/__init__.py
@@ -0,0 +1,39 @@
+"""
+Pydantic Schemas for API validation
+"""
+from .user import UserBase, UserCreate, UserResponse, UserUpdate
+from .document import (
+    DocumentBase, DocumentCreate, DocumentSaveRequest, 
+    DocumentResponse, DocumentWithExtractions
+)
+from .extraction import (
+    ExtractionBase, ExtractionCreate, ExtractionResponse,
+    ExtractedField, TemplateExtractionResponse,
+    ParsedPageResponse, GeneralExtractionResponse
+)
+from .common import UploadURLResponse, ErrorResponse
+
+__all__ = [
+    # User
+    "UserBase",
+    "UserCreate", 
+    "UserResponse",
+    "UserUpdate",
+    # Document
+    "DocumentBase",
+    "DocumentCreate",
+    "DocumentSaveRequest",
+    "DocumentResponse",
+    "DocumentWithExtractions",
+    # Extraction
+    "ExtractionBase",
+    "ExtractionCreate",
+    "ExtractionResponse",
+    "ExtractedField",
+    "TemplateExtractionResponse",
+    "ParsedPageResponse",
+    "GeneralExtractionResponse",
+    # Common
+    "UploadURLResponse",
+    "ErrorResponse",
+]
