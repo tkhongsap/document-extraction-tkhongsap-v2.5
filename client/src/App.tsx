8803f6b2e762ca7ac4f403f2acbdbf9792e507d6
Atiwat <abs.atiwat@gmail.com>
2026-01-12 12:32:59 +0700

    fix(toast): add Sonner Toaster for extraction page notifications
    - Import Toaster from sonner as SonnerToaster
    - Add SonnerToaster component with position='bottom-right' and richColors
    - Now toast notifications show properly on extraction page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { Switch, Route, Redirect } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
+import { Toaster as SonnerToaster } from "sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { useAuth } from "@/hooks/useAuth";
 import { Layout } from "@/components/layout";
@@ -118,6 +119,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
+        <SonnerToaster position="bottom-right" richColors />
         <Router />
       </TooltipProvider>
     </QueryClientProvider>
